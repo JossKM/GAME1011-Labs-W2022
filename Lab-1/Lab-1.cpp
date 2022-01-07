4792c2b20b9afb39f4b32105d6a38d94e14feaef
JossKM <jossmooyoung@gmail.com>
2022-01-06 21:30:43 -0500

    Lab 1 starting content

---
+++ Lab-1/Lab-1.cpp
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////
+// GAME1011 Lab 1
+////////////////////////////////////////////////////////////////
+ 
+
+/////// Topic: Classes, instantiation, pointers, and arrays
+
+/////// 1: Setup
+// Create your own repo or clone from https://github.com/JossKM/GAME1011-Labs-W2022                                                                                         
+
+/////// 2: Circle Class
+// What information is required to define a circle?
+// Constructor
+// getArea() - Accessor
+// setArea() - Mutator
+
+/////// 3: Rectangle Class
+// What information is required to define a rectangle?
+// Constructor
+// getArea() - Accessor
+// setArea() - Mutator
+
+//Shapes
+//Circle
+//Rectangle
+//Get area function
+//Constructors
+//Member variables/ data members
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+#include <iostream>
+
+int main()
+{
+}
+
